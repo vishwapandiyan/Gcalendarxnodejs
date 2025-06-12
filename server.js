5a519de2f5d36b8a2d9a67bc7e1628aeb79b28f2
Vishwa P <162886244+vishwapandiyan@users.noreply.github.com>
2025-06-12 22:41:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
-// Load environment variables from .env file
+
 require('dotenv').config();
 
 const express = require('express');
 const app = express();
 
-// Import route handlers
+
 const authRoutes = require('./routes/authRoutes');
 
 // Middleware
 app.use(express.json());
 
-// Mount routes
+// routes
 app.use('/', authRoutes); 
 
-// Health check route
+
 app.get('/', (req, res) => {
-  res.send('Google Calendar API is running ✅');
+  res.send('Google Calendar API is running ');
 });
 
-// Start the server
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`✅ Server is running on http://localhost:${PORT}`);
+  console.log(` Server is running on http://localhost:${PORT}`);
 });
